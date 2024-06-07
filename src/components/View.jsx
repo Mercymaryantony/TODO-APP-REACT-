c0f01f42a608af81d7df8edf2235573cdb65ea09
Mercymaryantony <mercyantony1112@gmail.com>
2024-06-08 01:47:56 +0530

    changes made

--- src/components/View.jsx
+++ src/components/View.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import Navbar from './Navbar'
 
 const View = () => {
     const [data,chnagedata]=useState(
@@ -1207,6 +1208,7 @@ const View = () => {
     )
   return (
     <div>
+        <Navbar/>
         <div className="container">
             <div className="row">
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
